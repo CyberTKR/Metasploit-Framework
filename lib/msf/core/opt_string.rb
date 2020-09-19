ae1d6485039bd88367cef27ac5e81fdb4782bbe5
Your Name <you@example.com>
2020-09-20 01:10:26 +0300

    Metasploit rapid7

---
+++ lib/msf/core/opt_string.rb
@@ -0,0 +1,46 @@
+# -*- coding: binary -*-
+
+module Msf
+
+###
+#
+# Mult-byte character string option.
+#
+###
+class OptString < OptBase
+
+  # This adds a length parameter to check for the maximum length of strings.
+  def initialize(in_name, attrs = [],
+               required: false, desc: nil, default: nil, enums: [], regex: nil, aliases: [], max_length: nil)
+    super
+  end
+
+  def type
+    return 'string'
+  end
+
+  def validate_on_assignment?
+    true
+  end
+
+  def normalize(value)
+    if (value.to_s =~ /^file:(.*)/)
+      path = $1
+      begin
+        value = File.read(path)
+      rescue ::Errno::ENOENT, ::Errno::EISDIR
+        value = nil
+      end
+    end
+    value
+  end
+
+  def valid?(value=self.value, check_empty: true)
+    value = normalize(value)
+    return false if check_empty && empty_required_value?(value)
+    return false if invalid_value_length?(value)
+    return super(value, check_empty: false)
+  end
+end
+
+end
